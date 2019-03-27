060e49c2f7c5b443da30a1da963511f7d579f623
Michal_M <mmielczarek12@gmail.com>
2019-03-27 15:46:39 +0100

    registration on web  page

--- MyApp-SPA/src/app/nav/nav.component.ts
+++ MyApp-SPA/src/app/nav/nav.component.ts
@@ -14,8 +14,8 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
-  login(){
-    this.authService.login(this.model).subscribe( 
+  login() {
+    this.authService.login(this.model).subscribe(
       next => {
         console.log('Logged in successfully');
       },
@@ -23,6 +23,16 @@ export class NavComponent implements OnInit {
         console.log('Failed to login');
       }
     );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
