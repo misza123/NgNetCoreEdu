24a7b5ca98e1fed13547933ac44efc704448d716
Michal_M <mmielczarek12@gmail.com>
2019-03-28 14:15:05 +0100

    angular global error handling

--- MyApp.API/Users/Dtos/UserForRegisterDTO.cs
+++ MyApp.API/Users/Dtos/UserForRegisterDTO.cs
@@ -8,7 +8,7 @@ namespace MyApp.API.Users.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8.")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters.")]
         public string Password { get; set; }
     }
 }
